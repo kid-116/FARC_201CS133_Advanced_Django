6b707aa75ec63791b61ab2c8dafc75c9eed42089
kid-116 <mehultodi116@gmail.com>
2021-05-30 23:21:21 +0530

    removed routines

--- app/iTMS/iTMS/settings.py
+++ app/iTMS/iTMS/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'sections',
-    'routines',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
