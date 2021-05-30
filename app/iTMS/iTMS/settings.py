6e1f43f6848f60c7b3b9d53dfdbe556650c3cffa
kid-116 <mehultodi116@gmail.com>
2021-05-30 11:47:43 +0530

    created models for timetable

--- app/iTMS/iTMS/settings.py
+++ app/iTMS/iTMS/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'sections',
+    'timetables',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ app/iTMS/timetables/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.db.models.fields import TimeField
+from sections.models import Section
+
+class Timetable(models.Model):
+    id = models.AutoField(primary_key=True)
+    section = models.ForeignKey(Section, related_name='timetable', blank=False, null=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return "Timetable for " + self.section.name
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, blank=False, null=False)
+    starts_at = models.DateTimeField(blank=False, null=False)
+    ends_at = models.DateTimeField(blank=True, null=True)
+    venue = models.CharField(max_length=30, blank=False, null=False)
+    desc = models.CharField(max_length=100, blank=True, verbose_name="Description")
+    created_at = models.DateTimeField(auto_now_add=True)
+    table = models.ForeignKey(Timetable, blank=False, on_delete=models.CASCADE, null=False)
+
+class Period(models.Model):
+    DAYS = (
+        (1, 'MONDAY'),
+        (2, 'TUESDAY'),
+        (3, 'WEDNESDAY'),
+        (4, 'THURSDAY'),
+        (5, 'FRIDAY'),
+        (6, 'SATURDAY'),
+        (7, 'SUNDAY')
+    )
+    id = models.AutoField(primary_key=True)
+    subject = models.CharField(max_length=30, blank=False, null=False)
+    starts_at = models.TimeField(blank=False, null=False)
+    ends_at = models.TimeField(blank=False, null=False)
+    venue = models.CharField(max_length=30,blank=True, null=True)
+    day = models.PositiveSmallIntegerField(choices=DAYS)
+    table = models.ForeignKey(Timetable, blank=False, null=False, on_delete=models.CASCADE)
+
